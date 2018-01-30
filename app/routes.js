2149bcc09804c25f1fcc65a9dd942d606504a191
Ko-Hsin Chu <chukohsin@gmail.com>
2018-01-29 23:02:38 -0500

    let me pull

--- app/routes.js
+++ app/routes.js
@@ -5,6 +5,8 @@ import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
 import D3 from './components/D3'
 import WhatIsYourLine from './components/WhatIsYourLine'
 import NotFound from './components/NotFound'
+import SingleStopList from './components/SingleStopList'
+
 const auth = firebase.auth()
 ///test
 export default class Routes extends Component {
@@ -14,7 +16,8 @@ export default class Routes extends Component {
                 <Router>
                     <div>
                     <Route exact path='/' component={WhatIsYourLine} />
-                    <Route path='/:line' component={D3} />
+                    <Route exact path='/:line/:stopID' component={SingleStopList} />
+                    <Route exact path='/:line' component={D3} />
                     {/* <Route path='/*' component={NotFound}/> */}
                     </div>
                 </Router>
