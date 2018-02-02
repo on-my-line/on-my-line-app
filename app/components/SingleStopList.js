f57c1f556d00f5d204afba58ccb28bbf7d35f8f8
Leah Gerald <Leanator13@gmail.com>
2018-02-02 11:26:08 -0500

    default img and flex added to single stop pg

--- app/components/SingleStopList.js
+++ app/components/SingleStopList.js
@@ -2,6 +2,21 @@ import React, { Component } from 'react'
 import {connect} from 'react-redux'
 import { GridList, GridTile } from 'material-ui/GridList';
 //import { setStop } from '../store'
+import IconButton from "material-ui/IconButton";
+import Subheader from "material-ui/Subheader";
+import StarBorder from "material-ui/svg-icons/toggle/star-border";
+const styles = {
+	root: {
+		display: "flex",
+		flexWrap: "wrap",
+		justifyContent: "space-around"
+	},
+	gridList: {
+		width: 500,
+		height: 450,
+		overflowY: "auto"
+	}
+};
 
 const mapState = (state) => {
     return{
@@ -33,40 +48,55 @@ class SingleStopList extends Component {
             let meetupThings = this.props.meetup.filter( thing => {
                 return thing.stopId === stop
             })
-            return (
-                <div>
-                    <h1>Things to do near {singleStop[0].properties.STOP_NAME}</h1>
-                <GridList
-                    cols={1}
-                    cellHeight={300}
-                >
-                {yelpThings.map(thing => 
-                    <GridTile 
-                    key={thing.id}
-                    // onClick={(e) => {
-                    //   e.preventDefault
-                    //  route to single page
-                    // }}
-                    title={thing.name}
-                    subtitle={<span>Rating: {thing.rating}</span>}
-                    >
-                    {(thing.img) ? <img src={thing.img}/> : ""}
-                    </GridTile>)}
-                {meetupThings.map(thing => 
-                    <GridTile 
-                    key={thing.id}
-                    // onClick={(e) => {
-                    //   e.preventDefault
-                    //  route to single page
-                    // }}
-                    title={thing.name}
-                    subtitle={<span>Rating: {thing.rating}</span>}
-                    >
-                    {(thing.img) ? <img src={thing.img}/> : ""}
-                    </GridTile>)}
-                </GridList>
-                </div>
-        )}
+   
+            return <div style={styles.root}>
+								<GridList cellHeight={180} style={styles.gridList}>
+									<Subheader>
+										{`Things to do near: ${
+											singleStop[0].properties.STOP_NAME
+										}`}
+									</Subheader>
+									{yelpThings.map(thing => (
+										<GridTile
+											key={thing.id}
+											// onClick={(e) => {
+											//   e.preventDefault
+											//  route to single page
+											// }}
+											title={thing.name}
+											subtitle={<span>Rating: {thing.rating}</span>}
+											actionIcon={
+												<IconButton>
+													<StarBorder color="white" />
+												</IconButton>
+											}
+										>
+											{thing.img ? (
+												<img src={thing.img} />
+											) : (
+												<img src="https://yt3.ggpht.com/a-/AK162_53TCkRV0sl6Bx6OpTBE49CVTtyNoJyazMZFg=s900-mo-c-c0xffffffff-rj-k-no" />
+											)}
+										</GridTile>
+									))}
+									{meetupThings.map(thing => (
+										<GridTile
+											key={thing.id}
+											// onClick={(e) => {
+											//   e.preventDefault
+											//  route to single page
+											// }}
+											title={thing.name}
+											subtitle={<span>Rating: {thing.rating}</span>}
+										>
+											{thing.img ? (
+												<img src={thing.img} />
+											) : (
+												<img src="https://thumb7.shutterstock.com/display_pic_with_logo/2117717/504799285/stock-photo-meeting-meetup-organization-text-concept-504799285.jpg" />
+											)}
+										</GridTile>
+									))}
+								</GridList>
+							</div>;}
         else{
             return (
                 <div>
