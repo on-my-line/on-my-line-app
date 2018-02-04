a36ae75c444db89210e2fe3007ac95b55f6a9e19
Leah Gerald <Leanator13@gmail.com>
2018-02-03 22:15:54 -0500

    hashtags

--- app/components/SingleYelpPage.js
+++ app/components/SingleYelpPage.js
@@ -7,12 +7,11 @@ export default function SingleYelpPage(props){
     return(
         <div>
             <h1><a href={currentThing.url}>{currentThing.name}</a></h1>
-            <h2>Rating: {currentThing.rating}</h2>
-            <h2>Price: {currentThing.price}</h2>
-            <h2>categories: </h2>{currentThing.category.map(type => {
-                return <h3>{type}</h3>
-            })
-            }    
+            {currentThing.rating? <h2>Rating: {currentThing.rating}</h2> : "" }
+            {currentThing.price? <h2>Price: {currentThing.price}</h2> : "" }
+            {currentThing.category.map(type => {
+                return <p key={type}>#{type}</p>
+            })}
             {currentThing.img? <img src={currentThing.img}/> : <img src=""/>}
             <h3>Address: {currentThing.location}</h3>
             <h3>Phone: {currentThing.phone}</h3>
