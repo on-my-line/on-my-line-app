90497946e60a3cf02304377908d73ac2240e8526
Sierra Saitta-Zelterman <ssaitta13@gmail.com>
2018-02-07 12:57:02 -0500

    tryig to deploy again

--- server/index.js
+++ server/index.js
@@ -1,13 +1,13 @@
 'use strict'
 const express = require('express')
 const path = require('path')
-const volleyball = require('volleyball')
+// const volleyball = require('volleyball')
 const bodyParser = require('body-parser')
 const PORT = 1337
 
 const app = express()
 
-app.use(volleyball)
+// app.use(volleyball)
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
