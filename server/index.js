3092ecb724283ed9f81ba01ac4d8d81ee3d4ca17
Sierra Saitta-Zelterman <ssaitta13@gmail.com>
2018-02-07 14:53:13 -0500

    changed port thing

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@
 const express = require('express')
 const path = require('path')
 const bodyParser = require('body-parser')
-const PORT = 1337
+const PORT = process.env.PORT || 1337
 
 const app = express()
 
